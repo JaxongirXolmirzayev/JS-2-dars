3ee5a06484fb19d9bc4dfb553ca25a88f38c56af
AhadullayevAbdulloh <147791264+JaxongirXolmirzayev@users.noreply.github.com>
2023-10-18 18:08:04 +0500

    JS 2-dars
    27-guruh

---
+++ JS 2-dars/JS/main.js
@@ -0,0 +1,87 @@
+/*
+ if     (agar)
+ else if    (yoki bo'lmasa)
+ else   (u holda)
+*/
+
+// if ("Salom" == "Salom"){
+//     console.log("Ular teng!");
+// }
+// else {
+//     console.log("Ular teng emas!");
+// }
+
+// if  (45 == 45){
+//     console.log("Ular teng!-1");
+// }
+// else if (45 == 45){
+//     console.log("Ular teng!-2");
+// }
+// else{
+//     console.log("Yuqoridegilar hech biri teng emas!");
+// }
+
+
+// if  (77 == 64){
+//     console.log("Javob to'g'ri!-1");
+// }
+// else if (72 == 72){
+//     console.log("Teng   ");
+// }
+// else{
+//     console.log("Yuqoridegilar hech biri teng emas!");
+// }
+//
+// if  ("IT darisi soat-6da boshlanadi"){
+//     console.log("Ha to'g'ri ");
+// }
+// else if ("IT darisi soat-6da boshlanadi"){
+//     console.log("Ha javob to'g'ri");
+// }
+// else{
+//     console.log("Ikkala javob ham to'g'ri");
+// }
+//
+// if ("IT JS 2-darsmi"){
+//     console.log("Ha 2-dars");
+// }
+// else {
+//     console.log("To'g'ri Javob");
+// }
+//
+// if ("IT darsi qiziqarli == IT darsi qiziqarli "){
+//     console.log("Ha Qiziq dars");
+// }
+// else {
+//     console.log("To'g'ri Javob");
+// }
+
+// let a =10;
+// let b =46;
+// let c =49;
+// let d =55;
+
+// if (a == 10 && b == 46 && c == 49 && d == 55){
+//     console.log("Hech qaysini o'zgartirmasa to'g'ri bo'ladi");
+// }
+// else {
+//     console.log("1 ta donasi javobini o'zgartirsa  hammasini noto'g'ri bo'ladi");
+// }
+
+// if (a == 10 || b == 46 || c == 49 || d == 55){
+//     console.log("Qiymatlarni kamida bittasi to'gri bo'lsa u to'gri")
+// }
+// else{
+//     console.log("Qiymatlarni hammasi notog'ri")
+// }
+
+/* Data types in JS: undefined,boolean*/
+
+// let matn = "JavaScript"      /* String */
+// let son = 45                 /* Number */
+// let text;                    /* Undefined */
+// let rost = true;             /* Boolean*/
+
+let rost = 45 ==45 ? true:false;
+
+document.write( rost)
